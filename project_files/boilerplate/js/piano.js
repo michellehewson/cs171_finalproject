95e2527420cce7fe8751eec463a1892f35436bc8
michellehewson <michellelhewson@gmail.com>
2023-11-07 11:25:53 -0500

    fixing piano keys

--- project_files/boilerplate/js/piano.js
+++ project_files/boilerplate/js/piano.js
@@ -1,22 +1,17 @@
 // michelle
 
-let whiteKeys = [
-    1,3,5,6,8,10,12
-];
+let whiteKeys = [1, 3, 5, 6, 8, 10, 12];
 
-
-let pianoMargin = {top: 40, right: 10, bottom: 60, left: 60};
+let pianoMargin = { top: 40, right: 10, bottom: 60, left: 60 };
 let pianoWidth = 960 - pianoMargin.left - pianoMargin.right;
-let pianoHeight = 700 - pianoMargin.top - pianoMargin.bottom;
+let pianoHeight = 400 - pianoMargin.top - pianoMargin.bottom;
 let keyWidth = pianoWidth / whiteKeys.length;
 
-//console.log(keyWidth);
-
 let svgPiano = d3.select("#piano").append("svg")
-    .attr("pianoWidth", pianoWidth + pianoMargin.left + pianoMargin.right)
-    .attr("pianoHeight", pianoHeight + pianoMargin.top + pianoMargin.bottom)
+    .attr("width", pianoWidth + pianoMargin.left + pianoMargin.right)
+    .attr("height", pianoHeight + pianoMargin.top + pianoMargin.bottom)
     .append("g")
-  //  .attr("transform", "translate(" + pianoMargin.left + "," + pianoMargin.top + ")");
+    .attr("transform", "translate(" + pianoMargin.left + "," + pianoMargin.top + ")");
 
 let pianoWhiteKeys = svgPiano.selectAll(".pianoWhiteKey")
     .data(whiteKeys)
@@ -24,7 +19,6 @@ let pianoWhiteKeys = svgPiano.selectAll(".pianoWhiteKey")
     .append("rect")
     .attr("class", "pianoWhiteKey")
     .attr("x", function (d, i) {
-        console.log(i * keyWidth)
         return i * keyWidth;
     })
     .attr("y", 0)
