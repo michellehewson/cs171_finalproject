f144aa3323776c2e55fc55c956810b5c08002274
Soline Boussard <soline.boussard@gmail.com>
2023-11-10 20:56:23 -0500

    fixed radar plot

--- project_files/boilerplate/js/radarchart.js
+++ project_files/boilerplate/js/radarchart.js
@@ -7,8 +7,6 @@ class RadarChart {
 
     }
 
-
-
     initVis() {
         let vis = this;
         const NUM_OF_SIDES = 5;
@@ -23,22 +21,28 @@ class RadarChart {
             y: size / 2
         };
 
+        const colorScale = d3.scaleOrdinal(d3.schemeCategory10); // You can choose a different color scheme
+
+
         const tooltip = d3.select(".tooltip");
 
-        const desiredColumns = ['danceability', 'energy', 'speechiness', 'instrumentalness', 'liveness'];
+        const desiredColumns = ['danceability', 'energy', 'speechiness', 'acousticness', 'liveness'];
 
         // Fetch Spotify data and extract relevant columns
-        const spotifySubset = vis.spotifyData.slice(0, 1);
+        const spotifySubset = vis.spotifyData.slice(0, 2);
 
         const dataset = [];
 
 // Iterate over each attribute and create the dataset
-        desiredColumns.forEach(attribute => {
-            const data = spotifySubset.map(entry => ({
-                name: attribute,
-                value: entry[attribute] // Use the actual attribute value from the data
-            }));
-            dataset.push(...data);
+        spotifySubset.forEach(row => {
+            const data = {
+                track: row.track_name,
+                values: desiredColumns.map(attribute => ({
+                    name: attribute,
+                    value: row[attribute]
+                }))
+            };
+            dataset.push(data);
         });
 
         // Scale for mapping data to chart size
@@ -84,7 +88,7 @@ class RadarChart {
 
         // Iterate over each attribute in the dataset and create points
         desiredColumns.forEach((attribute, i) => {
-            const attributeValue = dataset[i].value; // Assuming dataset has been correctly constructed
+            const attributeValue = dataset[0].values[i].value; // Access data correctly
             const theta = i * polyangle;
 
             points.push(generatePoint({
@@ -93,44 +97,34 @@ class RadarChart {
             }));
         });
 
-        const drawPath = (points, parent, strokeColor = "black") => {
+        const drawPath = (points, parent, strokeColor = "black", fillColor = "none", fillOpacity = 1) => {
             const lineGenerator = d3.line()
                 .x(d => d.x)
                 .y(d => d.y);
 
             parent.append("path")
                 .attr("d", lineGenerator(points))
-                .attr("fill", "none") // Set fill to none
-                .attr("stroke", strokeColor); // Set stroke color
+                .attr("fill", fillColor)
+                .attr("fill-opacity", fillOpacity)
+                .attr("stroke", strokeColor);
         };
 
-        points = [...points, points[0]];
-        drawPath(points, g, "black"); // Set stroke color to black
 
-        const generateAndDrawLevels = ( levelsCount, sideCount ) =>
-        {
+        const generateAndDrawLevels = (levelsCount, sideCount) => {
+            const levelsGroup = g.append("g").attr("class", "levels-group");
 
-            for ( let level = 1; level <= levelsCount; level++ )
-            {
-                const hyp = ( level / levelsCount ) * r_0;
+            for (let level = 1; level <= levelsCount; level++) {
+                const hyp = (level / levelsCount) * r_0;
 
                 const points = [];
-                for ( let vertex = 0; vertex < sideCount; vertex++ )
-                {
+                for (let vertex = 0; vertex < sideCount; vertex++) {
                     const theta = vertex * polyangle;
 
-                    points.push( generatePoint( { length: hyp, angle: theta } ) );
-
+                    points.push(generatePoint({ length: hyp, angle: theta }));
                 }
-                const group = g.append( "g" ).attr( "class", "levels" );
-                drawPath([...points, points[0]], group, "black"); // Set stroke color to black
-
+                drawPath([...points, points[0]], levelsGroup, "black"); // Set stroke color to black
             }
-
-
         };
-
-        generateAndDrawLevels( NUM_OF_LEVEL, NUM_OF_SIDES );
 
         const generateAndDrawLines = ( sideCount ) =>
         {
@@ -146,16 +140,19 @@ class RadarChart {
 
         };
 
+        generateAndDrawLines(NUM_OF_SIDES);
+        generateAndDrawLevels(NUM_OF_LEVEL, NUM_OF_SIDES);
+        points = [...points, points[0]];
+        drawPath(points, g, "black", "lightblue", 0.3);
 
-        generateAndDrawLevels( NUM_OF_LEVEL, NUM_OF_SIDES );
-        generateAndDrawLines( NUM_OF_SIDES );
-        const drawCircles = points => {
+
+        const drawCircles = (points) => {
             const mouseEnter = (event, d) => {
                 tooltip.style("opacity", 1);
                 const { x, y } = d3.pointer(event);
                 tooltip.style("top", `${y - 20}px`);
                 tooltip.style("left", `${x + 15}px`);
-                tooltip.text(d.value);
+                tooltip.html(`<strong>Track:</strong> ${dataset[0].track}<br><strong>Data Point:</strong> ${d.value}`);
             };
 
             const mouseLeave = () => {
@@ -170,36 +167,44 @@ class RadarChart {
                 .append("circle")
                 .attr("cx", d => d.x)
                 .attr("cy", d => d.y)
-                .attr("r", 8)
-                .on("mouseenter", mouseEnter)
+                .attr("r", 4)
+                .on("mouseenter", (event, d) => mouseEnter(event, d))
                 .on("mouseleave", mouseLeave);
         };
 
-        const drawData = ( dataset, n ) =>
-        {
-            const points = [];
-            dataset.forEach( ( d, i ) =>
-            {
-                const len = scale( d.value );
-                const theta = i * ( 2 * Math.PI / n );
+        const drawData = (dataset, n) => {
+            // Create a group for paths
+            const pathsGroup = g.append("g").attr("class", "shape-group");
 
-                points.push(
-                    {
-                        ...generatePoint( { length: len, angle: theta } ),
-                        value: d.value
-                    } );
-            } );
+            // Create a group for circles
+            const circlesGroup = g.append("g").attr("class", "circles-group");
 
-            const group = g.append( "g" ).attr( "class", "shape" );
+            dataset.forEach((d, i) => {
+                const trackGroup = g.append("g").attr("class", "track-group"); // Create a group for each track
 
-            drawPath( [ ...points, points[ 0 ] ], group );
-            drawCircles( points );
+                let points = []; // Move points array inside the loop
+
+                d.values.forEach((value, j) => {
+                    const len = scale(value.value);
+                    const theta = j * (2 * Math.PI / n);
+
+                    points.push(generatePoint({ length: len, angle: theta }));
+
+                    // Append path to the paths group
+                    const pathGroup = pathsGroup.append("g").attr("class", "shape");
+                    drawPath([...points, points[0]], pathGroup, "black", colorScale(d.track), 0.5);
+
+                    // Append circles to the circles group
+                    const circleGroup = circlesGroup.append("g").attr("class", "indic");
+                    drawCircles(points, circleGroup, d.track);
+
+                    // Append circles to the track group
+                    const circleGroupTrack = trackGroup.append("g").attr("class", "indic");
+                    drawCircles(points, circleGroupTrack, d.track);
+                });
+            });
         };
 
-        generateAndDrawLevels( NUM_OF_LEVEL, NUM_OF_SIDES );
-        generateAndDrawLines( NUM_OF_SIDES );
-        //drawAxis( ticks, NUM_OF_LEVEL );
-        drawData( dataset, NUM_OF_SIDES );
 
         const drawText = ( text, point, isAxis, group ) =>
         {
@@ -229,24 +234,21 @@ class RadarChart {
 
         };
 
-        const drawLabels = ( dataset, sideCount ) =>
-        {
-            const groupL = g.append( "g" ).attr( "class", "labels" );
-            for ( let vertex = 0; vertex < sideCount; vertex++ )
-            {
-
+        const drawLabels = (dataset, sideCount) => {
+            const groupL = g.append("g").attr("class", "labels");
+            for (let vertex = 0; vertex < sideCount; vertex++) {
                 const angle = vertex * polyangle;
-                const label = dataset[ vertex ].name;
-                const point = generatePoint( { length: 0.9 * ( size / 2 ), angle } );
+                const label = dataset[0].values[vertex].name; // Adjust the way you access the name property
+                const point = generatePoint({ length: 0.9 * (size / 2), angle });
 
-                drawText( label, point, false, groupL );
+                drawText(label, point, false, groupL);
             }
         };
 
-        generateAndDrawLevels( NUM_OF_LEVEL, NUM_OF_SIDES );
-        generateAndDrawLines( NUM_OF_SIDES );
         //drawAxis( ticks, NUM_OF_LEVEL );
         drawData( dataset, NUM_OF_SIDES );
+
+
         drawLabels( dataset, NUM_OF_SIDES );
 
         vis.wrangleData();
