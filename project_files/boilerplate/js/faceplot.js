b78e810d5b494841117b418e8de61f549a32ea33
Soline Boussard <soline.boussard@gmail.com>
2023-11-15 19:54:18 -0500

    added spotidy heads

--- project_files/boilerplate/js/faceplot.js
+++ project_files/boilerplate/js/faceplot.js
@@ -4,20 +4,17 @@ class FacePlot {
         this.spotifyData = spotifyData;
         this.tiktokData = tiktokData;
         this.initVis();
+
     }
 
     showTracksForArtist(selectedArtist) {
-        // Filter the full TikTok dataset for rows matching the selected artist
         const artistTracks = this.tiktokData.filter(entry => entry.artist_name === selectedArtist);
-
-        // Display track names in the artist-name-container
         const tracksHtml = artistTracks.map(entry => `<p>${entry.track_name}</p>`).join('');
         d3.select('#artist-name-container').html(`<h4>Tracks for ${selectedArtist}:</h4>${tracksHtml}`);
-
-        // Update your visualization as needed
     }
 
     initVis() {
+        console.log("Initializing visualization");
         let vis = this;
 
         vis.margin = { top: 40, right: 10, bottom: 60, left: 60 };
@@ -32,44 +29,35 @@ class FacePlot {
             .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
         const rows = 3;
-         vis.cols = 3;
+        vis.cols = 3;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+        // Calculate the width and height of each cell
          vis.cellSize = {
             width: vis.width / vis.cols - 50,
-=======
-=======
-
->>>>>>> 86dfcc5e0d26d688e9faecee235ddd0649e2c4ca
-        // Calculate the width and height of each cell
-       // console.log("new face", vis.tiktokSubset)
-
-        const cellSize = {
-            width: vis.width / cols - 50,
-<<<<<<< HEAD
->>>>>>> 8c6c7cd8eab2429fc5b47627f809475d40ad0762
-=======
->>>>>>> 86dfcc5e0d26d688e9faecee235ddd0649e2c4ca
             height: vis.height / rows
         };
 
-        d3.select('#showTopArtistsButton').on('click', function () {
-            // Toggle between TikTok and Spotify data
-            if (vis.isShowingTikTok) {
-                vis.showTopArtists();
-            } else {
-                vis.getTopArtistsFromSpotify();
-            }
+        d3.select('#showTopArtistsButton').text('Show Spotify Data');
 
-            // Update the visualization
+        d3.select('#showTopArtistsButton').on('click', function () {
+            console.log('Button Clicked');
+            if (vis.isShowingTikTok) {
+                vis.getTopArtistsFromSpotify();
+                d3.select(this).text('Show TikTok Data'); // Change button text
+            } else {
+                vis.getTopArtistsFromTiktok();
+                d3.select(this).text('Show Spotify Data'); // Change button text
+            }
+            console.log('Subset after getTopArtistsFromSpotify:', vis.subset);
+
             vis.updateVis();
         });
 
-        vis.showTopArtists();
+
+        vis.getTopArtistsFromTiktok();
 
         const cells = vis.svg.selectAll('.cell')
-            .data(vis.subset)
+            .data(vis.subset) // Make sure vis.subset is defined
             .enter()
             .append('g')
             .attr('class', 'cell')
@@ -82,7 +70,16 @@ class FacePlot {
                 return `translate(${translateX},${translateY})`;
             });
 
-// Add a pattern for each artist's face image
+        vis.defs = vis.svg.append('defs');
+
+        vis.defs.append('filter')
+            .attr('id', 'drop-shadow')
+            .attr('height', '130%')
+            .append('feDropShadow')
+            .attr('dx', 0)
+            .attr('dy', 4)
+            .attr('stdDeviation', 4);
+
         const patterns = cells.append('defs')
             .append('pattern')
             .attr('id', (d, i) => `pattern-${i}`)
@@ -96,31 +93,24 @@ class FacePlot {
             .attr('height', 120) // Adjust the height to match the circle's radius * 2
             .attr('xlink:href', d => `img/${d}.png`);
 
-// Add a circle for each artist's face image
-
         cells.append('circle')
             .attr('r', 60) // Adjust the radius as needed
-            .style('fill', (d, i) => `url(#pattern-${i})`) // Reference the pattern using the index
+            .style('fill', (d, i) => `url(#pattern-${i})`)
             .style('stroke', 'black')
             .style('stroke-width', '2')
             .on('mouseover', function (event, d) {
-                // Display artist's name and tracks when hovering over the circle
                 d3.select(this)
                     .style('filter', 'url(#drop-shadow)'); // Apply shadow filter
 
                 d3.select('#artist-name-container').text(d);
-
-                // Show tracks for the artist
                 vis.showTracksForArtist(d);
             })
             .on('mouseout', function () {
-                // Clear the artist's name and remove shadow when not hovering
                 d3.select(this)
                     .style('filter', null); // Remove shadow filter
                 d3.select('#artist-name-container').text('');
             })
             .on('click', function (event, d) {
-                // Handle click event to show track names for the selected artist
                 vis.showTracksForArtist(d);
             });
 
@@ -133,26 +123,22 @@ class FacePlot {
             .attr('dy', 4)
             .attr('stdDeviation', 4);
 
-    }
-
-    showTopArtists() {
-        let vis = this;
-        vis.isShowingTikTok = true;
-
-        // Your logic to display the top 9 TikTok artists goes here
-        vis.tiktokData.sort((a, b) => b.artist_pop - a.artist_pop);
-        vis.uniqueArtists = Array.from(new Set(vis.tiktokData.map(entry => entry.artist_name)));
-        vis.subset = vis.uniqueArtists.slice(0, 9);
-
-        // Update the visualization
+        // Initialize the subset and update the visualization
         vis.updateVis();
     }
 
+    getTopArtistsFromTiktok() {
+        let vis = this;
+        vis.isShowingTikTok = true;
+        vis.tiktokData.sort((a, b) => b.artist_pop - a.artist_pop);
+        vis.uniqueArtists = Array.from(new Set(vis.tiktokData.map(entry => entry.artist_name)));
+        vis.subset = vis.uniqueArtists.slice(0, 9);
+    }
+
     getTopArtistsFromSpotify() {
+        console.log("hereeee")
         let vis = this;
         vis.isShowingTikTok = false;
-
-        // Your logic to display the top 9 Spotify artists goes here
         vis.spotifyData.sort((a, b) => {
             if (a.peak_rank !== b.peak_rank) {
                 return a.peak_rank - b.peak_rank;
@@ -160,55 +146,93 @@ class FacePlot {
                 return b.weeks_on_chart - a.weeks_on_chart;
             }
         });
+        console.log('Subset after sorting spotifyData:', vis.subset);
+
         vis.uniqueArtists = Array.from(new Set(vis.spotifyData.map(entry => entry.artist_name)));
         vis.subset = vis.uniqueArtists.slice(0, 9);
-        console.log("spotify", vis.subset)
-
-        // Update the visualization
-        vis.updateVis();
+        console.log(vis.subset)
     }
-
 
     updateVis() {
         let vis = this;
 
-        // Update the data binding for circles
-        const circles = vis.svg.selectAll('.cell circle')
+        // Update the data binding for cells
+        const cells = vis.svg.selectAll('.cell')
             .data(vis.subset);
 
         // Exit
-        circles.exit().remove();
+        cells.exit()
+            .transition() // Add transition for exit
+            .duration(500) // Set the duration of the transition in milliseconds
+            .remove();
 
-        // Enter
-        const newCircles = circles.enter()
-            .append('circle')
-            .attr('r', 60) // Adjust the radius as needed
-            .style('stroke', 'black')
-            .style('stroke-width', '2')
-            .on('mouseover', function (event, d) {
-                // Your existing mouseover logic
-            })
-            .on('mouseout', function () {
-                // Your existing mouseout logic
-            })
-            .on('click', function (event, d) {
-                // Your existing click logic
+        // Enter + Update
+        const newCells = cells.enter()
+            .append('g')
+            .merge(cells) // Merge the enter and update selections
+            .attr('class', 'cell')
+            .attr('transform', (d, i) => {
+                const colIndex = i % vis.cols;
+                const rowIndex = Math.floor(i / vis.cols);
+                const translateX = colIndex * vis.cellSize.width + vis.cellSize.width / 2;
+                const translateY = rowIndex * vis.cellSize.height + vis.cellSize.height / 2;
+
+                return `translate(${translateX},${translateY})`;
             });
 
-        // Update
-        circles.merge(newCircles)
-            .transition() // You can add transitions for a smoother update
-            .attr('cx', (d, i) => {
-                const colIndex = i % vis.cols;
-                return colIndex * vis.cellSize.width + vis.cellSize.width / 2;
-            })
-            .attr('cy', (d, i) => {
-                const rowIndex = Math.floor(i / vis.cols);
-                return rowIndex * vis.cellSize.height + vis.cellSize.height / 2;
-            })
-            .style('fill', (d, i) => `url(#pattern-${i})`);
+        // Append <defs> and <pattern> elements
+        const patterns = newCells.selectAll('.pattern')
+            .data(d => [d]);
 
-        // Additional update logic if needed
+        patterns.exit().remove(); // Remove unnecessary patterns
+
+        const newPatterns = patterns.enter()
+            .append('defs')
+            .append('pattern')
+            .attr('id', (d, i) => `pattern-${i}`)
+            .attr('class', 'pattern')
+            .attr('width', 1)
+            .attr('height', 1)
+            .merge(patterns) // Merge the enter and update selections
+            .select('image')
+            .attr('xlink:href', d => `img/${d}.png`);
+
+
+        newCells.select('circle')
+            .style('fill', (d, i) => `url(#pattern-${i})`)
+            .style('stroke', 'black')
+            .style('stroke-width', '2')
+            .transition() // Add transition for update
+            .duration(500)
+            .attr('r', 60) // Adjust the radius as needed
+            .on('mouseover', function (event, d) {
+                d3.select(this)
+                    .style('filter', 'url(#drop-shadow)') // Apply shadow filter
+                    .transition() // Add transition for mouseover
+                    .duration(200) // Set the duration of the transition in milliseconds
+                    .attr('r', 70);
+                d3.select('#artist-name-container').text(d);
+                vis.showTracksForArtist(d);
+            })
+            .on('mouseout', function () {
+                d3.select(this)
+                    .style('filter', null) // Remove shadow filter
+                    .transition() // Add transition for mouseout
+                    .duration(200) // Set the duration of the transition in milliseconds
+                    .attr('r', 60);
+                d3.select('#artist-name-container').text('');
+            })
+            .on('click', function (event, d) {
+                vis.showTracksForArtist(d);
+            });
+
+        vis.svg.append('defs')
+            .append('filter')
+            .attr('id', 'drop-shadow')
+            .attr('height', '130%')
+            .append('feDropShadow')
+            .attr('dx', 0)
+            .attr('dy', 4)
+            .attr('stdDeviation', 4);
     }
-
 }
